3706482282a0ad93d55bd3a7f9ce811368c492db
Damian Wasiak <damianwasiak49@gmail.com>
2021-08-31 19:08:44 +0200

    Add map elemets

--- front-end/src/Pages/Hodowle.js
+++ front-end/src/Pages/Hodowle.js
@@ -2,21 +2,47 @@ import React from 'react'
 import {Farm} from '../Components/Farm'
 const Hodowle=() =>{
     const ContainerStyle={display:"flex" ,width:"100%" , flexDirection: "row" ,alignItems: "flex-start", justifyContent: "center" ,flexWrap:"wrap"}
-    return(
+
+let Dane=[
+[
+  "hodowla krówek",
+   "./images/krowy.jpeg",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
+
+]
+,
+[
+    "hodowla maciorek",
+    "./images/swinki.jpeg",
+   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
+   
+   ],
+   [
+    "hodowla królików",
+     "./images/króliki.jpeg",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
+  ]
+  ,
+  [
+   "hodowla królików",
+    "./images/króliki.jpeg",
+       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
+ ],
+ [
+    "hodowla maciorek",
+    "./images/swinki.jpeg",
+   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
+   
+   ]
    
 
-
-    
+]
+    return(
+      
 
 <div  style={ContainerStyle}>
- <Farm Title="Chodowla Krówek" image="./images/krowy.jpeg"  description="O kurwaaa" />
- <Farm Title="Chodowla Krówek" image="./images/krowy.jpeg"  description="O kurwaaa" /> 
-  <Farm Title="Chodowla Krówek" image="./images/krowy.jpeg"  description="O kurwaaa" />
-  <Farm Title="Chodowla Krówek" image="./images/krowy.jpeg"  description="O kurwaaa" />
-  <Farm Title="Chodowla Krówek" image="./images/krowy.jpeg"  description="O kurwaaa" />
-  <Farm Title="Chodowla Krówek" image="./images/krowy.jpeg"  description="O kurwaaa" />
-  <Farm Title="Chodowla Krówek" image="./images/krowy.jpeg"  description="O kurwaaa" />
-  
+    
+    {Dane.map(items => <Farm Title={items[0]} image={items[1]}  description={items[2]} />)}
 
   </div>
     
