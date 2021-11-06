f33af8d3fc29946a0eace9bced3abb51f9ee2a3c
Damian Wasiak <damianwasiak49@gmail.com>
2021-11-06 13:32:47 +0100

    deploy repair Images

--- client/src/Pages/Hodowle.js
+++ client/src/Pages/Hodowle.js
@@ -1,50 +1,57 @@
 import React from 'react'
 import {Farm} from '../Components/Farm'
 import {Newfarm} from '../Components/Newfarm'
+import kroliki from '../Assets/Images/kroliki.jpeg'
+import krowy from '../Assets/Images/krowy.jpeg'
+import Owczarki from '../Assets/Images/Owczarki.jpeg'
+import swinki from '../Assets/Images/swinki.jpeg'
+
+
+
 const Hodowle=() =>{
     const ContainerStyle={display:"flex" ,width:"100%" , flexDirection: "row" ,alignItems: "flex-start", justifyContent: "center" ,flexWrap:"wrap"}
 
 let Dane=[
 [
   "hodowla krówek",
-   "./images/krowy.jpeg",
+  krowy,
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
 
 ]
 ,
 [
     "hodowla maciorek",
-    "./images/swinki.jpeg",
+    swinki,
    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
    
    ],
    [
     "hodowla królików",
-     "./images/króliki.jpeg",
+    kroliki,
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
   ]
   ,
   [
    "hodowla królików",
-    "./images/króliki.jpeg",
+   kroliki,
        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
  ],
  [
     "hodowla maciorek",
-    "./images/swinki.jpeg",
+    swinki,
    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
    
    ]
    ,
  [
     "hodowla owczarków niemieckich",
-    "./images/Owczarki.jpeg",
+    Owczarki,
    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
    
    ],
    [
     "hodowla krówek",
-     "./images/krowy.jpeg",
+    krowy,
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the lea"
   
   ]
